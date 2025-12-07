5f7d18fb20a30a56df975cbef5cb2fdf028ee952
Иванов Кирилл Евгеньевич <ivanov.ke72@gmail.com>
2025-12-07 23:53:53 +0300

    Задания к ЛР_1

---
+++ ЛР_1_Задание 1.py
@@ -0,0 +1,11 @@
+
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+total_sum = 0
+for number in numbers:
+    if number is not None:
+        total_sum += number
+len_of_numbers = len(numbers)
+average_of_numbers = total_sum / len_of_numbers
+numbers [4] = average_of_numbers
+print("Измененный список:",numbers)
+
---
+++ ЛР_1_Задание 3.py
@@ -0,0 +1,6 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+total_players = len(list_players)//2
+first_team = list_players[:total_players]
+second_team = list_players[total_players:]
+print(first_team)
+print(second_team)
