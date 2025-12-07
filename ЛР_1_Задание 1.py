8d9b94d8c0fe321427bc265f2e16e0b83390f4e6
Иванов Кирилл Евгеньевич <ivanov.ke72@gmail.com>
2025-12-08 00:10:41 +0300

    Update ЛР_1_Задание 1.py

--- ЛР_1_Задание 1.py
+++ ЛР_1_Задание 1.py
@@ -8,3 +8,12 @@ average_of_numbers = total_sum / len_of_numbers
 numbers [4] = average_of_numbers
 print("Измененный список:",numbers)
 
+"""
+(Через исключение индекса не получалось получить верное значение для ср.арифм. значения, поэтому пришлось использовать цикл "for")
+skip_index = 4
+sum_of_numbers = sum(numbers[:skip_index])
+len_of_numbers = len(numbers)
+average_of_numbers = sum_of_numbers / len_of_numbers
+numbers [4] = average_of_numbers
+print("Измененный список:",numbers)
+"""
