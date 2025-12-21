84673fe6065cce9910fe70b76e14eb286b046c9e
Иванов Кирилл Евгеньевич <ivanov.ke72@gmail.com>
2025-12-21 19:37:13 +0300

    ЛР_2_Задание 2.py

--- ЛР_2_Задание 2.py
+++ ЛР_2_Задание 2.py
@@ -1,8 +1,20 @@
-salary = 5000  # Ежемесячная зарплата
-spend = 6000  # Траты за первый месяц
-months = 10  # Количество месяцев, которое планируется протянуть без долгов
-increase = 0.03  # Ежемесячный рост цен
+salary = 5000     # Ежемесячная зарплата
+spend = 6000      # Траты за первый месяц
+months = 10       # Количество месяцев
+increase = 0.03   # Ежемесячный рост цен
 
-# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+money_capital = 0
+current_spend = spend
 
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", ...)
+for month in range(1, months + 1):
+    # Если зарплаты не хватает
+    if current_spend > salary:
+        money_capital += current_spend - salary
+
+    # Увеличиваем расходы на следующий месяц
+    current_spend = current_spend * (1 + increase)
+
+# Округляем до целого
+money_capital = round(money_capital)
+
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
